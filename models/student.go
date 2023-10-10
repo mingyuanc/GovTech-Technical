c16ecafca74a9f748e3db737611fc0fe877639fc
mingyuanc <cmingyuan123@gmail.com>
2023-10-10 18:44:53 +0800

    Require checks on model

--- models/student.go
+++ models/student.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Student struct {
 	gorm.Model
-	Email    string     `json:"email" gorm:"unique;not null"`
+	Email    string     `json:"email" binding:"required,email" gorm:"unique;not null"`
 	Teachers []*Teacher `json:"teachers" gorm:"many2many:teacher_student;"`
 }
--- models/teacher.go
+++ models/teacher.go
@@ -6,6 +6,6 @@ import (
 
 type Teacher struct {
 	gorm.Model
-	Email    string     `json:"email" gorm:"unique;not null"`
+	Email    string     `json:"email" binding:"required,email" gorm:"unique;not null"`
 	Students []*Student `json:"students" gorm:"many2many:teacher_student;"`
 }
